5888099c5fa7598a69927f0db2eca2be7bb62d30
tariqzile114 <tariqzile114@gmail.com>
2025-11-26 22:11:48 -0500

    Create day1_exercises.py
    Pratice basic Python variables, data types, arithmatic operations and combining strings

---
+++ Day1_Python_Basics/day1_exercises.py
@@ -0,0 +1,30 @@
+#Day 1: Python Basics Exercises
+
+# 1. Create variables
+name = "Tina"
+age = 45
+savings = 7567.84
+
+# 2. Print them
+print("Name:", name)
+print("Age:", age)
+print("Savings:", savings)
+
+# 3. Print age next year
+print("Age next year:", age + 1)
+
+# 4. Arithmetic operations
+num1 = 10
+num2 = 3
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Division:", num1 / num2)
+print("Remainder:", num1 % num2)
+
+#5. Combine Strings
+first_name = "Tina"
+last_name = "Sani"
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
