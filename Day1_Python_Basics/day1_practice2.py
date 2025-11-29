29d05e5865f1edc50faa25afdc4616a39d0c1019
tariqzile114 <tariqzile114@gmail.com>
2025-11-28 22:00:45 -0500

    day1_practice2.py
    Added additional Day 1 practice exercises including variable operations, f-strings, and basic type conversion.

---
+++ Day1_Python_Basics/day1_practice2.py
@@ -0,0 +1,22 @@
+fav_color = "Green"
+goal_salary = 10_000
+is_learning_python = True
+
+print(fav_color)
+print(goal_salary)
+print(is_learning_python)
+
+print("My favorite color is", fav_color, "I want to earn", goal_salary, "and learning Python is", is_learning_python)
+
+# Use f_string
+print(f"My Favorite color is {fav_color}, I want to earn {goal_salary}, and learning Python is {is_learning_python}.")
+
+# Convert minutes to hours
+minutes = 120
+hours = minutes / 60
+print("Hours:", hours)
+
+# Convert number to string
+number = 120
+conv_to_string = str(number)
+print("The number is", conv_to_string)
